fcbcc3f710bfa6db05fe8e08035c814aa7c87b38
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-11-20 15:56:57 -0300

    ajuste lib core

--- src/test/java/com/glaiss/users/UsersApplicationTests.java
+++ src/test/java/com/glaiss/users/UsersApplicationTests.java
@@ -1,13 +1,8 @@
 package com.glaiss.users;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UsersApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
