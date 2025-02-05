bad2d8602570903723c7dabe281ba3630e146ca0
Gustavo Acácio <gustavo.acacio@attus.ai>
2025-02-05 19:20:38 -0300

    adicionando logica de adicionar item a lista de item

--- src/test/java/com/glaiss/users/client/lista/ListaServiceImplMock.java
+++ src/test/java/com/glaiss/users/client/lista/ListaServiceImplMock.java
@@ -1,10 +1,12 @@
 package com.glaiss.users.client.lista;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Primary
 public class ListaServiceImplMock implements ListaService {
     @Override
     public Boolean deletar(UUID id) {
