37956a358488a801e53c6eb9f75a9599ba6fe33b
Gustavo Acácio <gustavo.acacio@attus.ai>
2025-02-06 10:22:29 -0300

    adicionando testes

--- src/test/java/com/glaiss/users/UsersApplicationTest.java
+++ src/test/java/com/glaiss/users/UsersApplicationTest.java
@@ -2,18 +2,15 @@ package com.glaiss.users;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.NestedTestConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.TimeZone;
 
 @SpringBootTest(classes = UsersApplication.class)
 @NestedTestConfiguration(NestedTestConfiguration.EnclosingConfiguration.OVERRIDE)
-@ExtendWith(SpringExtension.class)
 @WithMockUser(username = "userId=c0a7b5f5-fc77-4afe-bf73-8213b3862bfb, username=galasdalas50@gmail.com",
         authorities = {"ROLE_ADMIN"})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
